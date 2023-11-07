1fc2193c33ab5b56fd7b43cd49e7e744b08465b8
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-11-06 18:57:53 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-  word = ""
-  if not my_string:
-      return None
-  for i in my_string:
-    if i != 'c' and i != 'C':
-      word = word + i
-  return word 
+    """
+    removes all instances of 'c' & 'C' from string
+    """
+    new_str = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_str += my_string[i]
+    return (new_str)
