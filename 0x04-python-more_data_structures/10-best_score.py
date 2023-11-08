ea11b71127c293cbc85ef4497f6805e981a3430f
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-11-08 06:40:36 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     max = 0
     best_score = ''
     for i, j in a_dictionary.items():
-       if max < j:
-           max = j
-           best_score = i
+        if max < j:
+            max = j
+            best_score = i
     return best_score
