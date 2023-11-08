ce4069ac6c97a8a7c29e7d7d8542dc6fc9b347e7
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-11-08 06:25:44 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     max = 0
     best_score = ''
+    if a_dictionary is None:
+        return None
     for i in a_dictionary.keys():
        if max < a_dictionary[i]:
            max = a_dictionary[i]
