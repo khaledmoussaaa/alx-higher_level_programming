4b332059c5b710a3bd4fd2031bf487a6b5574be4
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-11-08 12:42:24 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string != None:
+    if not isinstance(roman_string, str):
         return 0
     ruman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
