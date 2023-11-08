e7de873315ae02922f8c022934b06407620997e9
khaledmoussaaa <khaledmoussa909@gmail.com>
2023-11-08 09:34:07 -0800

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) != str or roman_string != None:
+        return 0
+    ruman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    total = 0
+    for i in reversed(roman_string):
+        num = ruman[i]
+        if total < num * 5:
+            total += num
+        else:
+            num = -num
+            total += num
+    return total
