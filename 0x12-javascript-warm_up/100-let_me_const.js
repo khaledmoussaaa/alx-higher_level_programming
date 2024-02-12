588a16b0445d57f0c54cb4199f0378fd01a43adc
Khaled Moussa <133710558+khaledmoussaaa@users.noreply.github.com>
2024-02-13 01:04:15 +0200

    Update 100-let_me_const.js

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+this.executeXTimes = function(x, theFunction) {
+    for (let i = 0; i < x; i++) {
+        theFunction();
+    }
+};
+function myFunction() {
+    console.log("Executing myFunction");
+}
+
+executeXTimes(5, myFunction);
