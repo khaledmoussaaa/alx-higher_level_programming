d86464763868a64d67d1e705fbf019161132d0b3
Khaled Moussa <133710558+khaledmoussaaa@users.noreply.github.com>
2024-02-13 01:20:05 +0200

    Update 13-add.js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,3 +1,15 @@
 #!/usr/bin/node
+// 13-add.js
+
+// Define an object with a method named add
+const mathFunctions = {};
+
+// Add a method to the object to add two integers
+mathFunctions.add = function(a, b) {
+    return a + b;
+};
+
+// Export the object containing the add method
+module.exports = mathFunctions;
 const add = require('./13-add').add;
 console.log(add(3, 5));
